367a084cae8775d46eaedd47a3e7821b0b80aafe
ollo69 <60491700+ollo69@users.noreply.github.com>
2020-07-02 22:34:30 +0200

    Fix cover state

--- custom_components/tuya_custom/tuyaha/devices/cover.py
+++ custom_components/tuya_custom/tuyaha/devices/cover.py
@@ -3,20 +3,21 @@ from .base import TuyaDevice
 
 class TuyaCover(TuyaDevice):
 
+    def state(self):
+        state = self.data.get("state")
+        return state
+
     def open_cover(self):
         """Open the cover."""
-        if self._control_device("turnOnOff", {"value": "1"}):
-            self._update_data("state", True)
+        self._control_device("turnOnOff", {"value": "1"})
 
     def close_cover(self):
         """Close cover."""
-        if self._control_device("turnOnOff", {"value": "0"}):
-            self._update_data("state", False)
+        self._control_device("turnOnOff", {"value": "0"})
 
     def stop_cover(self):
         """Stop the cover."""
-        if self._control_device("startStop", {"value": "0"}):
-            self._update_data("state", False)
+        self._control_device("startStop", {"value": "0"})
 
     def support_stop(self):
         support = self.data.get("support_stop")
