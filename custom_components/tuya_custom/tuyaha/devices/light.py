64e1023177141e11c9170e7b4829eb6753f15caa
nao-pon <hypweb@gmail.com>
2020-07-04 16:29:15 +0900

    Adjust brightness scale
    The light goes out when the brightness value falls below about 10%, so
    the scale is changed to take it into account.

--- custom_components/tuya_custom/tuyaha/devices/light.py
+++ custom_components/tuya_custom/tuyaha/devices/light.py
@@ -1,5 +1,9 @@
 from .base import TuyaDevice
 
+"""The minimum brightness value set in the API that does not turn off the light."""
+MIN_BRIGHTNESS_VAL = 10.3
+MIN_BRIGHTNESS = 255 * MIN_BRIGHTNESS_VAL / 100
+
 class TuyaLight(TuyaDevice):
 
     def brightness(self):
@@ -7,7 +11,9 @@ class TuyaLight(TuyaDevice):
         if work_mode == "colour" and "color" in self.data:
             brightness = int(self.data.get("color").get("brightness") * 255 / 100)
         else:
-            brightness = self.data.get("brightness")
+            brightness = int(self.data.get("brightness"))
+        """Rescale with MIN_BRIGHTNESS as "1"."""
+        brightness = min(int(max((brightness - MIN_BRIGHTNESS), 1) / (255 - MIN_BRIGHTNESS) * 255), 255)
         return brightness
 
     def _set_brightness(self, brightness):
@@ -62,7 +68,12 @@ class TuyaLight(TuyaDevice):
 
     def set_brightness(self, brightness):
         """Set the brightness(0-255) of light."""
-        value = int(brightness * 100 / 255)
+        if (int(brightness) > 0):
+            """Restore the value rescaled with MIN_BRIGHTNESS."""
+            brightness = min(max(int(brightness / 255 * (255 - MIN_BRIGHTNESS) + MIN_BRIGHTNESS), MIN_BRIGHTNESS), 255)
+            value = max(brightness * 100 / 255.0, MIN_BRIGHTNESS_VAL)
+        else:
+            brightness = value = 0
         if self._control_device("brightnessSet", {"value": value}):
             self._update_data("brightness", brightness)
 
