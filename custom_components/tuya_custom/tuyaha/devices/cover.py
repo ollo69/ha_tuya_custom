acd685f40edf61ab0f1f9cbff287441e4ee1f46a
ollo69 <60491700+ollo69@users.noreply.github.com>
2020-07-04 18:03:14 +0200

    Cover status management fix

--- custom_components/tuya_custom/tuyaha/devices/cover.py
+++ custom_components/tuya_custom/tuyaha/devices/cover.py
@@ -10,17 +10,17 @@ class TuyaCover(TuyaDevice):
     def open_cover(self):
         """Open the cover."""
         if self._control_device("turnOnOff", {"value": "1"}):
-            self._update_data("state", "1")
+            self._update_data("state", 1)
 
     def close_cover(self):
         """Close cover."""
         if self._control_device("turnOnOff", {"value": "0"}):
-            self._update_data("state", "2")
+            self._update_data("state", 2)
 
     def stop_cover(self):
         """Stop the cover."""
         if self._control_device("startStop", {"value": "0"}):
-            self._update_data("state", "0")
+            self._update_data("state", 3)
 
     def support_stop(self):
         support = self.data.get("support_stop")
