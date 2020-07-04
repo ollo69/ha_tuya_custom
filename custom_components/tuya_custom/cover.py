e1a1e36005f526a2a1290a0fec18d762a11b456a
ollo69 <60491700+ollo69@users.noreply.github.com>
2020-07-04 18:36:17 +0200

    Additional cover status fix

--- custom_components/tuya_custom/cover.py
+++ custom_components/tuya_custom/cover.py
@@ -58,6 +58,7 @@ class TuyaCover(TuyaDevice, CoverEntity):
         super().__init__(tuya, platform)
         self.entity_id = ENTITY_ID_FORMAT.format(tuya.object_id())
         self._was_closing = False
+        self._was_opening = False
 
     @property
     def supported_features(self):
@@ -72,6 +73,7 @@ class TuyaCover(TuyaDevice, CoverEntity):
         """Return if the cover is opening or not."""
         state = self._tuya.state()
         if state == 1:
+            self._was_opening = True
             self._was_closing = False
             return True
         return False
@@ -81,6 +83,7 @@ class TuyaCover(TuyaDevice, CoverEntity):
         """Return if the cover is closing or not."""
         state = self._tuya.state()
         if state == 2:
+            self._was_opening = False
             self._was_closing = True
             return True
         return False
@@ -89,13 +92,15 @@ class TuyaCover(TuyaDevice, CoverEntity):
     def is_closed(self):
         """Return if the cover is closed or not."""
         state = self._tuya.state()
-        return state != 2 and self._was_closing
-        # state = self._tuya.state()
+        if state != 2 and self._was_closing:
+            return True
+        if state != 1 and self._was_opening:
+            return False
         # if state == 1:
         #     return False
         # if state == 2:
         #     return True
-        # return None
+        return None
 
     def open_cover(self, **kwargs):
         """Open the cover."""
@@ -107,6 +112,7 @@ class TuyaCover(TuyaDevice, CoverEntity):
 
     def stop_cover(self, **kwargs):
         """Stop the cover."""
-        if not self.is_closed:
+        if self.is_closed is None:
+            self._was_opening = False
             self._was_closing = False
         self._tuya.stop_cover()
